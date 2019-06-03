5b7f88999cf69ab068edad9fc2d0ca812a523d9d
Michael Trenker <michael.trenker@htlpinkafeld.at>
2019-06-03 17:06:29 +0200

    update models

--- newbaeckerexpress/models/product.js
+++ newbaeckerexpress/models/product.js
@@ -2,11 +2,16 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var schema = new Schema({
-    imagePath: {type: String, required: true},
-    title: {type: String, required: true},
-    description: {type: String, required: true},
-    price: {type: Number, required: true},
-    category: {type: String, required: true}
+    imagePath: { type: String, required: true },
+    subimage1Path: { type: String, required: false },
+    subimage2Path: { type: String, required: false },
+    subimage3Path: { type: String, required: false },
+    subimage4Path: { type: String, required: false },
+    subimage5Path: { type: String, required: false },
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    category: { type: String, required: true }
 });
 
 module.exports = mongoose.model('Product', schema);
--- newbaeckerexpress/models/user.js
+++ newbaeckerexpress/models/user.js
@@ -3,16 +3,24 @@ var Schema = mongoose.Schema;
 var bcrypt = require('bcrypt-nodejs');
 
 var userSchema = new Schema({
-    email: {type: String, required: true},
-    password: {type: String, required: true}
+    firstname: { type: String, required: true },
+    lastname: { type: String, required: true },
+    city: { type: String, required: true },
+    zipcode: { type: Number, required: true },
+    street: { type: String, required: true },
+    number: { type: Number, required: true },
+    stair: { type: Number, required: false },
+    door: { type: Number, required: false },
+    email: { type: String, required: true },
+    password: { type: String, required: true }
 });
 
 userSchema.methods.encryptPassword = function(password) {
-  return bcrypt.hashSync(password, bcrypt.genSaltSync(5), null);  
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(5), null);
 };
 
 userSchema.methods.validPassword = function(password) {
-  return bcrypt.compareSync(password, this.password);  
+    return bcrypt.compareSync(password, this.password);
 };
 
 module.exports = mongoose.model('User', userSchema);
